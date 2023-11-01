23be5601f7c1b381050740b55cf3e46d1c682c49
NoriCheng <149048659+NoriCheng@users.noreply.github.com>
2023-11-01 15:29:35 +0800

    Update 1_StockOrganize.py

--- Data_processing/1_StockOrganize.py
+++ Data_processing/1_StockOrganize.py
@@ -1,10 +1,12 @@
 import mplfinance as mpf
+import pandas as pd
+
 df_2330_all=pd.DataFrame()
 def 處理日期(x):
     x=str(int(x[0:3])+1911)+x[3:]
     return x
 for n in range(7):
-    file = fr'C:\Users\USER\Desktop\Python\2330\STOCK_DAY_2330_2023{n+1:02d}.csv'
+    file = fr'2330\STOCK_DAY_2330_2023{n+1:02d}.csv'
     df_2330 = pd.read_csv(file,encoding="big5",header=1,skipfooter=6,engine='python',thousands=',')
     df_2330 = df_2330[['日期','開盤價','最高價','最低價','收盤價','成交股數']] \
                 .rename(columns={'日期':'Date','開盤價':'Open',
